abc47411709c2c0acb34016a94d89e6683bac951
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-10-24 15:31:41 -0300

    Added moti and created animation on Messages

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { MotiView } from 'moti';
 import { UserPhoto } from '../UserPhoto';
 
 import { styles } from './styles';
@@ -19,7 +20,12 @@ type Props = {
 
 export function Message({ data }: Props) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: 'timing', duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>
         {data.text}
       </Text>
@@ -34,7 +40,7 @@ export function Message({ data }: Props) {
         </Text>
       </View>
 
-    </View>
+    </MotiView>
 
 
   );
