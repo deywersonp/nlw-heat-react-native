115ec363d15931e16d039e53039dd21f57443e11
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-11-06 17:59:01 -0300

    Added function to add new send messages on real time / Added utils/messages to simulate a queue of messages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
 import { api } from '../../services/api';
+import { io } from 'socket.io-client';
+
+import { MESSAGES_EXAMPLE } from '../../utils/messages';
+
 import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
+
+let messagesQueue: MessageProps[] = MESSAGES_EXAMPLE;
+
+const socket = io(String(api.defaults.baseURL));
+socket.on('new_message', (newMessage) => {
+  messagesQueue.push(newMessage);
+});
+
 
 export function MessageList() {
   const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
@@ -15,6 +27,21 @@ export function MessageList() {
     }
 
     fetchMessages();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ]);
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
   }, []);
 
   return (
